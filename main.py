5ffb0e55de8b251a904d06451fb2e4276496f000
angelinag114 <167546506+angelinag114@users.noreply.github.com>
2024-05-17 10:59:57 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from character import Character
 
 pygame.init()
 pygame.font.init()
@@ -7,10 +8,13 @@ pygame.display.set_caption("  What happened?")
 
 size = (800, 600)
 screen = pygame.display.set_mode(size)
-
+transparent = (0, 0, 0, 0)
 background = pygame.image.load("map.png")
 
-welcome = my_font.render("What happened in this abandoned city?", True, (255, 255, 255))
+character=  Character(50,50)
+
+welcome = my_font.render("What happened in this abandoned city?", True,
+                         (255, 255, 255))
 start = my_font.render("Press space to start.", True, (255, 255, 255))
 
 keys = pygame.key.get_pressed()
@@ -18,13 +22,20 @@ screen.blit(background, (0, 0))
 screen.blit(welcome, (200, 230))
 screen.blit(start, (300, 250))
 run = True
-# while run:
-#   keys = pygame.key.get_pressed()
-#   if keys[pygame.K_SPACE]:
+
+while run:
+  keys = pygame.key.get_pressed()
+  if keys[pygame.K_SPACE]:
+    screen.blit(background, (0, 0))
+
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
+      game_start = False
+    if event.type == pygame.MOUSEBUTTONUP:
+      game_start = True
 
 
+    pygame.display.update()
 
-
-pygame.display.update()
-
-
+pygame.quit()
