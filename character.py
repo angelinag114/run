ca2b3bf4bcafb164b4ca989da643a58dfecdf112
angelinag114 <167546506+angelinag114@users.noreply.github.com>
2024-05-21 11:02:45 -0400

    Update character.py

--- character.py
+++ character.py
@@ -5,17 +5,16 @@ class Character:
   def __init__(self, x, y):
     self.x = x
     self.y = y
-    self.image_list = ["frontfacing.png", "backfacing.png", "rightfacing.png", "leftfacing.png"]
-    self.image = pygame.image.load(self.image_list[0])
+    self.image = pygame.image.load("frontfacing.png")
     self.rescale_image(self.image)
     self.image_size = self.image.get_size()
     self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
-    self.delta = 2
+    self.delta =  2
     self.front = True
 
   def rescale_image(self, image):
     self.image_size = self.image.get_size()
-    scale_size = (self.image_size[0] * .7, self.image_size[1] * .7)
+    scale_size = (self.image_size[0] * 2, self.image_size[1] * 2)
     self.image = pygame.transform.scale(self.image, scale_size)
 
   def move_direction(self, direction):
@@ -29,15 +28,4 @@ class Character:
          self.y = self.y + self.delta
      self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
 
-  def switch_image(self, direction):
-    image_number = 0
-    if not self.front and direction == "up":
-      image_number = 1
-      if not self.front and direction == "right":
-        image_number = 2
-        if not self.front and direction == " left":
-          image_number = 3
-    self.image = pygame.image.load(self.image_list[image_number])
-    self.rescale_image(self.image)
-    self.image_size = self.image.get_size()
-    self.front = not self.front
+
