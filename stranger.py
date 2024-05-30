45b0b1cd9040f259e200a13038457ae16411c369
angelinag114 <167546506+angelinag114@users.noreply.github.com>
2024-05-30 11:02:19 -0400

    Stranger sprite

---
+++ stranger.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Stranger:
+
+
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+    self.image = pygame.image.load("stranger.png")
+    self.rescale_image(self.image)
+    self.image_size = self.image.get_size()
+    self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
+    self.delta =  2
+    self.front = True
+
+  def rescale_image(self, image):
+    self.image_size = self.image.get_size()
+    scale_size = (self.image_size[0] * 2, self.image_size[1] * 2)
+    self.image = pygame.transform.scale(self.image, scale_size)
